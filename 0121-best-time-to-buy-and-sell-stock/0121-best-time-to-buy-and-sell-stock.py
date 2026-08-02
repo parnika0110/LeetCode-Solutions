a2bf220cbdfe4f464a6c67ca4506ba67687cd13a
Parnika S M <154581184+parnika0110@users.noreply.github.com>
2026-08-03 01:11:56 +0530

    Time: 43 ms (65.88%), Space: 28.7 MB (40.42%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
         min_price = prices[0]
+        max_profit = 0
         for i in range(len(prices)):
             if prices[i] < min_price:
                 min_price = prices[i]
@@ -9,4 +9,3 @@ class Solution:
             if profit > max_profit:
                 max_profit = profit
         return max_profit
-            
