c38370abe2add2cb577a5233e3dc9916ee0122ae
Parnika S M <154581184+parnika0110@users.noreply.github.com>
2026-08-07 02:33:04 +0530

    Time: 11 ms (77.29%), Space: 19.4 MB (46.19%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count1 = {}
+        count2 = {}
+        for char in s:
+            if char not in count1:
+                count1[char] = 0
+            count1[char] += 1
+        for char in t:
+            if char not in count2:
+                count2[char] = 0
+            count2[char] += 1
+        return count1 == count2
+        
