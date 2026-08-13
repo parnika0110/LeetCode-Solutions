020fcd2ac4cdf9bfe118d7df59e938a579833adc
Parnika S M <154581184+parnika0110@users.noreply.github.com>
2026-08-13 15:50:30 +0530

    Time: 262 ms (45.06%), Space: 35.3 MB (72.73%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -3,14 +3,10 @@ class Solution:
         count = {}
         left = 0
         ans = 0
-
         for right in range(len(nums)):
-            count[nums[right]] = count.get(nums[right], 0) + 1
-
+            count[nums[right]] = count.get(nums[right],0) + 1
             while count[nums[right]] > k:
                 count[nums[left]] -= 1
                 left += 1
-
             ans = max(ans, right - left + 1)
-
         return ans
